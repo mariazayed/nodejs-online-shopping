2fa6eb76e61d7679a5f047d5d7433e971f11bfef
Maria Zayed <mariawzayed@gmail.com>
2021-11-01 10:54:15 +0200

    Minor fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,4 @@
 const crypto = require('crypto');
-
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
