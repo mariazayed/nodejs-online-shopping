2cf0c6d432d0895f1711456286a7ebc4583a0df0
Maria Zayed <mariawzayed@gmail.com>
2021-11-12 16:36:44 +0200

    Refactoring

--- app.js
+++ app.js
@@ -99,8 +99,6 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  // res.status(error.httpStatusCode).render(...);
-  // res.redirect('/500');
   res.status(500).render('500', {
     pageTitle: 'Error!',
     path: '/500',
@@ -110,7 +108,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {
+  .then(() => {
     app.listen(3000);
   })
   .catch(err => {
