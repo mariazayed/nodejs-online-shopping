f3bd63bda4b44663d88dd5a05cab64a35e60301e
Maria Zayed <mariawzayed@gmail.com>
2021-11-15 16:14:11 +0200

    Finalizing

--- app.js
+++ app.js
@@ -99,10 +99,10 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.status(500).render('500', {
+	res.status(500).render('500', {
     pageTitle: 'Error!',
     path: '/500',
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: req.session?.isLoggedIn
   });
 });
 
